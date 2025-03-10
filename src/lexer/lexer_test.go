df7b7500d37ac4184fd7e02450c2ac2d73e7b7ab
Nuwan Rajika Kumarasiri <nuwan.kumarasiri@wisc.edu>
2025-03-10 08:16:11 -0500

    add query parser

---
+++ src/lexer/lexer_test.go
@@ -0,0 +1,27 @@
+package lexer
+
+import (
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+
+	tests := []string{
+		"courses?filter=equals(",
+		//"courses?filter=equals(displayName,'Brian Connor')",
+		//"students?filter=equals(displayName,null)",
+		//"teachers?filter=equals(displayName,lastName)",
+	}
+
+	for _, test := range tests {
+		l := New(test)
+
+		for {
+			tok := l.NextToken()
+			tok.Print()
+			if tok.Type == Eof {
+				break
+			}
+		}
+	}
+}
