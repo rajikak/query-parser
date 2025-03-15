53fdb60b14392c254d431caf98b7c648e1970768
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2025-03-15 14:54:28 -0500

    parser impl

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,15 +1,30 @@
 package parser
 
 import (
-	"go/token"
 	"query-parser/lexer"
 )
 
 type Parser struct {
 	l *lexer.Lexer
 
-	currentToken token.Token
-	peekToken    token.Token
+	currToken lexer.Token
+	peekToken lexer.Token
+}
 
-	//prefixParseFn map[token.Token]
+func New(l *lexer.Lexer) Parser {
+	return Parser{l, l.NextToken(), l.NextToken()}
+}
+
+func (p Parser) CheckToken(typ lexer.TokenType) bool {
+	return p.currToken.Type == typ
+}
+
+func (p Parser) CheckPeek(typ lexer.TokenType) bool {
+	return p.peekToken.Type == typ
+}
+
+func (p Parser) NextToken() lexer.Token {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+	return p.l.NextToken()
 }
